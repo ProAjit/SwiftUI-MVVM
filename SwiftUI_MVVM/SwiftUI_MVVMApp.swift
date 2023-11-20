5c339b4aa36dbeaffb15971409cccc7e7242afdf
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2023-11-20 14:36:21 +0530

    Initial Commit

---
+++ SwiftUI_MVVM/SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVMApp.swift
+//  SwiftUI_MVVM
+//
+//  Created by Ajit Satarkar on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserView()
+        }
+    }
+}
